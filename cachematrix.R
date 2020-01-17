d0aceb6da940661e4adbda9a97d78fba21db3a82
shahab-usf <59935529+shahab-usf@users.noreply.github.com>
2020-01-16 20:36:31 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below functions perform two activities. The makeCacheMatrix stores or caches the matrix. 
+## Whereas the cacheSolve function checks to see if the inverse of a matrix exists in the cache and if it doesn't it caculates the inverse
 
-## Write a short comment describing this function
 
 # The function makeCacheMatrix is intended to cache (or store) a matrix, 
 #ideally the inverse of a square matrix
@@ -14,28 +13,28 @@ makeCacheMatrix <- function(x = matrix()) {
     x <<- y
     m <<- NULL
   }
-  get <- function() x
-  setinv <- function(inv) m <<- inv
-  getinv <- function() m
+  get <- function() x #get the value
+  setinv <- function(inv) m <<- inv #set as the inverse
+  getinv <- function() m #get the inv as m
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
 # The fucntion cacheSolve is intended to either find the cache of a matrix if it exists, 
 # otherwise find the inverse of a matrix 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m <- x$getinv()
+  m <- x$getinv() #get inverse from cache
   if(!is.null(m)) {
     message("getting cached data")
-    return(m)
+    return(m) #return cached inv 
   }
   data <- x$get()
-  m <- solve(data, ...)
-  x$setmean(m)
+  m <- solve(data, ...) #solve for inverse if not there
+  x$getinv(m) 
   m
 }
